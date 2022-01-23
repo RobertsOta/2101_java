ce9e50f76b870a28a1927118a2c14660f189ee5e
RobertsOta <roberts.otankis@gmail.com>
2022-01-23 10:32:19 +0200

    Testing!

---
+++ src/com/lecture/Main.java
@@ -0,0 +1,10 @@
+package com.lecture;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        String sveiki = "Sveika pasaule";
+        System.out.println(sveiki);
+    }
+}
